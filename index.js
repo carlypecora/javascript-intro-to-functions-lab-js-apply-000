a1f6f122576bd7e48f0f9d492cf4d5e61361f561
Carly Pecora <carlyannpecora@gmail.com>
2019-03-22 19:42:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if(string != string.toUpperCase()){
-    return "I can't hear you!"
+  if(string == "I love you, Grandma"){
+    return "I love you, too."
   } else if(string == string.toUpperCase()){
     return "YES INDEED!"
   } else if(string == "I love you, Grandma"){
